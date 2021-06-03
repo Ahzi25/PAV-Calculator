ed09d3f71c8fa0e31369e61f37af7ff766f9740d
Paul Ahzi Villanueva <padvillanueva@iskolarngbayan.pup.edu.ph>
2021-06-03 15:06:50 +0800

    Add project files.

---
+++ PAV - Tracing Form/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace PAV___Tracing_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(80, 89);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(113, 39);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Contact Tracing Form";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 196);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Start);
+            this.Name = "Form1";
+            this.Text = "Contact Tracing Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
