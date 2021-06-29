46c5ab384a6603c1e927053e84521aee273d55ef
Paul Ahzi Villanueva <padvillanueva@iskolarngbayan.pup.edu.ph>
2021-06-30 00:43:09 +0800

    Calculator final

--- PAV - Calculator/Form1.cs
+++ PAV - Calculator/Form1.cs
@@ -18,68 +18,309 @@ namespace PAV___Calculator
         string first_num, second_num;
 
         public Form1()
-        { 
+        {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
         }
         private void Numbutton_Click(object sender, EventArgs e)
         {
-            Cal.GetNum();
+            Button button = (Button)sender;
+
+            if ((TotalDisplay.Text == "0") || (showprevoperation))
+                TotalDisplay.Clear();
+            showprevoperation = false;
+
+            if (button.Text == ".")
+            {
+                if (!TotalDisplay.Text.Contains("."))
+                    TotalDisplay.Text += button.Text;
+            }
+            else
+            {
+                TotalDisplay.Text += button.Text;
+            }
         }
 
         private void OperatorButton_Click(object sender, EventArgs e)
         {
-            Calculator.GetOperation();
+            Button op = (Button)sender;
+
+            if (resVal != 0)
+            {
+                EqualButton.PerformClick();
+                showprevoperation = true;
+                operand = op.Text;
+                prevOperation.Text = resVal + " " + operand;
+            }
+
+            else
+            {
+                operand = op.Text;
+                resVal = Double.Parse(TotalDisplay.Text);
+                showprevoperation = true;
+                prevOperation.Text = resVal + " " + operand;
+            }
+            first_num = prevOperation.Text;
         }
 
         private void EqualButton_Click(object sender, EventArgs e)
         {
-            Calculator.GetResult();
-        }
-
-        private void ReciprocalButton_Click(object sender, EventArgs e)
-        {
-            Calculator.GetReciprocal();
-        }
-
-        private void SquareButton_Click(object sender, EventArgs e)
-        {
-            Calculator.GetSquare();
+            switch (operand)
+            {
+                case "+":
+                    {
+                        TotalDisplay.Text = (resVal + Double.Parse(TotalDisplay.Text)).ToString();
+                        second_num = (Double.Parse(TotalDisplay.Text) - resVal).ToString();
+                        prevOperation.Text = first_num + " " + second_num + " " + "=";
+                        break;
+                    }
+                case "-":
+                    {
+                        TotalDisplay.Text = (resVal - Double.Parse(TotalDisplay.Text)).ToString();
+                        second_num = ((Double.Parse(TotalDisplay.Text) - resVal) * -1).ToString();
+                        prevOperation.Text = first_num + " " + second_num + " " + "=";
+                        break;
+                    }
+                case "×":
+                    {
+                        TotalDisplay.Text = (resVal * Double.Parse(TotalDisplay.Text)).ToString();
+                        second_num = (Double.Parse(TotalDisplay.Text) / resVal).ToString();
+                        prevOperation.Text = first_num + " " + second_num + " " + "=";
+                        break;
+                    }
+                case "÷":
+                    {
+                        TotalDisplay.Text = (resVal / Double.Parse(TotalDisplay.Text)).ToString();
+                        second_num = (resVal / Double.Parse(TotalDisplay.Text)).ToString();
+                        prevOperation.Text = first_num + " " + second_num + " " + "=";
+                        break;
+                    }
+                default:
+                    break;
+            }
+            resVal = Double.Parse(TotalDisplay.Text);
+            operand = "";
         }
 
         private void SquarerootButton_Click(object sender, EventArgs e)
         {
-            Calculator.GetSqareroot();
+            if (prevOperation.Text == resVal + " " + operand)
+            {
+                TotalDisplay.Text = (Math.Sqrt(Double.Parse(TotalDisplay.Text))).ToString();
+                switch (operand)
+                {
+                    case "+":
+                        {
+                            second_num = (Math.Round(Double.Parse(TotalDisplay.Text) * Double.Parse(TotalDisplay.Text))).ToString();
+                            prevOperation.Text = first_num + " " + "sqrt(" + second_num + ")" + " " + "=";
+                            TotalDisplay.Text = (resVal + Double.Parse(TotalDisplay.Text)).ToString();
+                            break;
+                        }
+                    case "-":
+                        {
+                            second_num = (Math.Round(Double.Parse(TotalDisplay.Text) * Double.Parse(TotalDisplay.Text))).ToString();
+                            prevOperation.Text = first_num + " " + "sqrt(" + second_num + ")" + " " + "=";
+                            TotalDisplay.Text = (resVal - Double.Parse(TotalDisplay.Text)).ToString();
+                            break;
+                        }
+                    case "×":
+                        {
+                            second_num = (Math.Round(Double.Parse(TotalDisplay.Text) * Double.Parse(TotalDisplay.Text))).ToString();
+                            prevOperation.Text = first_num + " " + "sqrt(" + second_num + ")" + " " + "=";
+                            TotalDisplay.Text = (resVal * Double.Parse(TotalDisplay.Text)).ToString();
+                            break;
+                        }
+                    case "÷":
+                        {
+                            second_num = (Math.Round(Double.Parse(TotalDisplay.Text) * Double.Parse(TotalDisplay.Text))).ToString();
+                            prevOperation.Text = first_num + " " + "sqrt(" + second_num + ")" + " " + "=";
+                            TotalDisplay.Text = (resVal / Double.Parse(TotalDisplay.Text)).ToString();
+                            break;
+                        }
+                    default:
+                        break;
+                }
+                resVal = Double.Parse(TotalDisplay.Text);
+                operand = "";
+            }
+            else
+            {
+                prevOperation.Text = "sqrt(" + TotalDisplay.Text + ")";
+                TotalDisplay.Text = (Math.Sqrt(Double.Parse(TotalDisplay.Text))).ToString();
+            }
+        }
+
+        private void SquareButton_Click(object sender, EventArgs e)
+        {
+            if (prevOperation.Text == resVal + " " + operand)
+            {
+                TotalDisplay.Text = (Double.Parse(TotalDisplay.Text) * Double.Parse(TotalDisplay.Text)).ToString();
+                switch (operand)
+                {
+                    case "+":
+                        {
+                            second_num = (Math.Sqrt(Double.Parse(TotalDisplay.Text))).ToString();
+                            prevOperation.Text = first_num + " " + "sqr(" + second_num + ")" + " " + "=";
+                            TotalDisplay.Text = (resVal + Double.Parse(TotalDisplay.Text)).ToString();
+                            break;
+                        }
+                    case "-":
+                        {
+                            second_num = (Math.Sqrt(Double.Parse(TotalDisplay.Text))).ToString();
+                            prevOperation.Text = first_num + " " + "sqr(" + second_num + ")" + " " + "=";
+                            TotalDisplay.Text = (resVal - Double.Parse(TotalDisplay.Text)).ToString();
+                            break;
+                        }
+                    case "×":
+                        {
+                            second_num = (Math.Sqrt(Double.Parse(TotalDisplay.Text))).ToString();
+                            prevOperation.Text = first_num + " " + "sqr(" + second_num + ")" + " " + "=";
+                            TotalDisplay.Text = (resVal * Double.Parse(TotalDisplay.Text)).ToString();
+                            break;
+                        }
+                    case "÷":
+                        {
+                            second_num = (Math.Sqrt(Double.Parse(TotalDisplay.Text))).ToString();
+                            prevOperation.Text = first_num + " " + "sqr(" + second_num + ")" + " " + "=";
+                            TotalDisplay.Text = (resVal / Double.Parse(TotalDisplay.Text)).ToString();
+                            break;
+                        }
+                    default:
+                        break;
+                }
+                resVal = Double.Parse(TotalDisplay.Text);
+                operand = "";
+            }
+            else
+            {
+                prevOperation.Text = "sqr(" + TotalDisplay.Text + ")";
+                TotalDisplay.Text = (Double.Parse(TotalDisplay.Text) * Double.Parse(TotalDisplay.Text)).ToString();
+            }
+        }
+
+
+        private void ReciprocalButton_Click(object sender, EventArgs e)
+        {
+            if (prevOperation.Text == resVal + " " + operand)
+            {
+                TotalDisplay.Text = (1 / Double.Parse(TotalDisplay.Text)).ToString();
+                switch (operand)
+                {
+                    case "+":
+                        {
+                            second_num = (1 / Double.Parse(TotalDisplay.Text)).ToString();
+                            prevOperation.Text = first_num + " " + "(1/" + second_num + ")" + " " + "=";
+                            TotalDisplay.Text = (resVal + Double.Parse(TotalDisplay.Text)).ToString();
+                            break;
+                        }
+                    case "-":
+                        {
+                            second_num = (1 / Double.Parse(TotalDisplay.Text)).ToString();
+                            prevOperation.Text = first_num + " " + "(1/" + second_num + ")" + " " + "=";
+                            TotalDisplay.Text = (resVal - Double.Parse(TotalDisplay.Text)).ToString();
+                            break;
+                        }
+                    case "×":
+                        {
+                            second_num = (1 / Double.Parse(TotalDisplay.Text)).ToString();
+                            prevOperation.Text = first_num + " " + "(1/" + second_num + ")" + " " + "=";
+                            TotalDisplay.Text = (resVal * Double.Parse(TotalDisplay.Text)).ToString();
+                            break;
+                        }
+                    case "÷":
+                        {
+                            second_num = (1 / Double.Parse(TotalDisplay.Text)).ToString();
+                            prevOperation.Text = first_num + " " + "(1/" + second_num + ")" + " " + "=";
+                            TotalDisplay.Text = (resVal / Double.Parse(TotalDisplay.Text)).ToString();
+                            break;
+                        }
+                    default:
+                        break;
+                }
+                resVal = Double.Parse(TotalDisplay.Text);
+                operand = "";
+            }
+            else
+            {
+                prevOperation.Text = ("1" + " " + "/" + " " + TotalDisplay.Text);
+                TotalDisplay.Text = (1 / Double.Parse(TotalDisplay.Text)).ToString();
+            }
         }
 
         private void PercentButton_Click(object sender, EventArgs e)
         {
-            Calculator.GetPercentage();
+            if (prevOperation.Text == resVal + " " + operand)
+            {
+                TotalDisplay.Text = (Double.Parse(TotalDisplay.Text) / 100).ToString();
+                switch (operand)
+                {
+                    case "+":
+                        {
+                            second_num = (100 * Double.Parse(TotalDisplay.Text)).ToString();
+                            prevOperation.Text = first_num + " " + second_num + "%" + " " + "=";
+                            TotalDisplay.Text = (resVal + Double.Parse(TotalDisplay.Text)).ToString();
+                            break;
+                        }
+                    case "-":
+                        {
+                            second_num = (100 * Double.Parse(TotalDisplay.Text)).ToString();
+                            prevOperation.Text = first_num + " " + second_num + "%" + " " + "=";
+                            TotalDisplay.Text = (resVal - Double.Parse(TotalDisplay.Text)).ToString();
+                            break;
+                        }
+                    case "×":
+                        {
+                            second_num = (100 * Double.Parse(TotalDisplay.Text)).ToString();
+                            prevOperation.Text = first_num + " " + second_num + "%" + " " + "=";
+                            TotalDisplay.Text = (resVal * Double.Parse(TotalDisplay.Text)).ToString();
+                            break;
+                        }
+                    case "÷":
+                        {
+                            second_num = (100 * Double.Parse(TotalDisplay.Text)).ToString();
+                            prevOperation.Text = first_num + " " + second_num + "%" + " " + "=";
+                            TotalDisplay.Text = (resVal / Double.Parse(TotalDisplay.Text)).ToString();
+                            break;
+                        }
+                    default:
+                        break;
+                }
+                resVal = Double.Parse(TotalDisplay.Text);
+                operand = "";
+            }
+            else
+            {
+                prevOperation.Text = (TotalDisplay.Text + "%");
+                TotalDisplay.Text = (Double.Parse(TotalDisplay.Text) / 100).ToString();
+            }
         }
 
         private void NegateButton_Click(object sender, EventArgs e)
         {
-            Calculator.GetNegative();
-        }
-
-        private void ClearButton_Click(object sender, EventArgs e)
-        {
-            Calculator.Clear();
-        }
-
-        private void ClearentryButton_Click(object sender, EventArgs e)
-        {
-            Calculator.ClearEntry();
-
+            TotalDisplay.Text = (0 - Double.Parse(TotalDisplay.Text)).ToString();
         }
 
         private void EraseButton_Click(object sender, EventArgs e)
         {
-            Calculator.Erase();
+            if (TotalDisplay.Text != "")
+                TotalDisplay.Text = TotalDisplay.Text.Remove(TotalDisplay.Text.Length - 1);
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            TotalDisplay.Text = "0";
+            prevOperation.Text = "";
+        }
+
+        private void ClearentryButton_Click(object sender, EventArgs e)
+        {
+            TotalDisplay.Text = "0";
+            resVal = 0;
+
         }
 
         private void Table1_Paint(object sender, PaintEventArgs e)
@@ -101,7 +342,5 @@ namespace PAV___Calculator
         {
 
         }
-
-
     }
 }
